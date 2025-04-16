f3d5ce0a1b0c4be9ecbd5d0099120a2fe73f708d
LPliar <gabrielstoffell01@gmail.com>
2025-04-16 17:09:07 -0300

    Add files via upload

---
+++ atv.py
@@ -0,0 +1,15 @@
+escolha = int(input("numero"))
+escolha1 = int(input("AAAAAAAAAA"))
+vt = escolha1
+at = escolha
+resultado1 = vt + at * at
+resultado2 = resultado1/2
+d = resultado2
+print(f"Resultado {resultado2}")
+
+#2 codigos 
+numero = 3.14
+raio = int(input("BOTA O RAIO SEU MERDa"))
+altura = int(input("BOTA A ALTURAAAAAAA"))
+resultado = numero*raio*raio*altura
+print(resultado)
