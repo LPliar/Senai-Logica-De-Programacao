5a7462a16e1bb64f0ab6c13420352c5adeeeeec2
LPliar <gabrielstoffell01@gmail.com>
2025-04-17 16:40:39 -0300

    Add files via upload

---
+++ tres.py
@@ -0,0 +1,77 @@
+
+#PRIMEIRO
+a = int(input("escreva o valor de A"))
+b = int(input("Valor de b"))
+c = int(input("digite o valor de c"))
+x = a/(b-c)
+print(a)
+print(b)
+print(c)
+print(x)
+
+print(f"o valor de x = a/b-c é {x}")
+
+#segundo
+ah = int(input("abacaxi"))
+v = int(input("234"))
+m = int(input(""))
+x = m * (ah + v*v/2 )
+
+print(x)
+
+
+#terceiro 
+a1 = "JANEIRO"
+a2 = "FEVEREIRO"
+a3 = "MArço"
+a4 = "Abril"
+a5 = "Maio"
+a6 = "Junho"
+a7 = "Julho"
+a8 = "AGosto"
+a9 = "Setembro"
+a10 = "outubro"
+a11 = "Novembro"
+a12 = "Dezembro"
+
+b1 = print("JANEIRO")
+b2 = print("FEVEREIRO") 
+b3 = print("MArço")
+b4 = print("Abrl")
+b5 = print("Maio") 
+b6 = print("Junho") 
+b7 = print("Julho")
+b8 = print("AGosto")
+b9 = print("Setembro")
+b10 = print("Outubro")
+b11 = print("Novembro")
+b12 = print("DEZEEEEEEEEEEEEEEMBROOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO")
+
+mes = int(input("Escolha um dos meses: "))
+
+if mes == 1:
+    print(a1)
+elif mes == 2:
+    print(a2)
+elif mes == 3:
+    print(a3)
+elif mes == 4:
+    print(a4)
+elif mes == 5:
+    print(a5)
+elif mes == 6:
+    print(a6)
+elif mes == 7:
+    print(a7)
+elif mes == 8:
+    print(a8)
+elif mes == 9:
+    print(a9)
+elif mes == 10:
+    print(a10)
+elif mes == 11:
+    print(a11)
+elif mes == 12:
+    print(a12)
+else: 
+    print("NONEEEEEEEEEEEEEE")
